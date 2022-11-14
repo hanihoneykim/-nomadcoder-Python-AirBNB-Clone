ee27f597d46468eadf70a50e9330397a675e8819
hanihoneykim <haeehani@gmail.com>
2022-11-14 20:24:47 +0900

    #11.3, #11.4 Rooms & Room Detail 강의 내용

--- experiences/urls.py
+++ experiences/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Perks, PerkDetail
+from . import views
 
 urlpatterns = [
-    path("perks/", Perks.as_view()),
-    path("perks/<int:pk>", PerkDetail.as_view()),
+    path("perks/", views.Perks.as_view()),
+    path("perks/<int:pk>", views.PerkDetail.as_view()),
 ]
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Amenities, AmenityDetail
+from . import views
 
 urlpatterns = [
-    path("amenities/", Amenities.as_view()),
-    path("amenities/<int:pk>", AmenityDetail.as_view()),
+    path("", views.Rooms.as_view()),
+    path("<int:pk>", views.RoomDetail.as_view()),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]

dbf5173ceacbac0f90b4b21882c107bfe272f89a
hanihoneykim <haeehani@gmail.com>
2022-11-14 19:00:42 +0900

    #11.0-#11.2 Amenity & Perk 강의 내용

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import Amenities, AmenityDetail
 
 urlpatterns = [
-    path("", views.see_all_rooms),
-    path("<int:room_pk>", views.see_one_room),
+    path("amenities/", Amenities.as_view()),
+    path("amenities/<int:pk>", AmenityDetail.as_view()),
 ]

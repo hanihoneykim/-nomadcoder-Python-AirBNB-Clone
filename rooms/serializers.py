32bf0ff1ecce7e58f17c9739d3474a511e1b5364
hanihoneykim <haeehani@gmail.com>
2022-11-16 02:00:42 +0900

    #11.14 Pagination & RoomDetail amenities put 수정

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -17,11 +17,9 @@ class AmenitySerializer(serializers.ModelSerializer):
 class RoomDetailSerializer(serializers.ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
-    amenities = AmenitySerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
-    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Room
